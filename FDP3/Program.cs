00299a653cd16ba40d6136375fe144523adf3a13
Irvin Martinez <Whateverh2001@gmail.com>
2020-02-01 19:16:59 -0400

    first commit

---
+++ FDP3/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Contactos = new string[10];
+            string[] NumDC = new string[10];
+
+            while (true)
+            {
+
+                bool close = false;
+                Console.Clear();
+                Console.Write("~ CONTACTOS ~\n" +
+                "\n Seleccione la accion que desea realizar" +
+                "\n [1] Agregar contacto" + "\n [2] Listar contactos" + "\n [3] Editar contacto" + "\n [4] Eliminar contacto" + "\n [5] Cerrar programa\n");
+                int Opcion = int.Parse(Console.ReadLine());
+
+                switch(Opcion)
+                {
+                    case 1:
+
+                        if (string.IsNullOrEmpty(Contactos[9]))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ingrese el nombre del contacto");
+                            string Name = Console.ReadLine();
+
+                            for (int i = 0; i < Contactos.Length; i++)
+                            {
+                                string elemento = Contactos[i];
+
+                                if (string.IsNullOrEmpty(elemento))
+                                {
+                                    Contactos[i] = Name;
+                                    Console.Clear();
+                                    break;
+
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hay mas espacio disponible");
+                            Console.ReadKey();
+                            break;
+                        }
+                        
+                        if(string.IsNullOrEmpty(NumDC[9]))
+                        {
+                            Console.WriteLine("Ingrese el numero telefonico del contato");
+                            string telefono = Convert.ToString(Console.ReadLine());
+                            for (int i = 0; i < NumDC.Length; i++)
+                            {
+                                string elemento = NumDC[i];
+
+                                if (string.IsNullOrEmpty(elemento))
+                                {
+                                    NumDC[i] = telefono;
+                                    Console.Clear();
+                                    break;
+
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hay mas espacio disponible");
+                            Console.ReadKey();
+                            break;
+                        }
+                        break;
+
+                    case 2:
+
+                        Console.Clear();
+                        Console.WriteLine("Contactos gruardados\n");
+                        for (int i = 0; i < Contactos.Length; i++)
+                        {
+                            string elemento = Contactos[i];
+                            Console.WriteLine((i + 1) + "-" + elemento);
+                                                    
+                        }
+                        Console.WriteLine("\nNumeros telefonicos de los contactos\n");
+                        for (int i = 0; i < NumDC.Length; i++)
+                        {
+                            string elemento2 = NumDC[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento2);
+
+                        }
+
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+
+                        Console.Clear();
+                        for (int i = 0; i < Contactos.Length; i++)
+                        {
+                            string elemento = Contactos[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento);
+                        }
+                        Console.WriteLine("\n");
+                        for (int i = 0; i < NumDC.Length; i++)
+                        {
+                            string elemento2 = NumDC[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento2);
+
+                        }
+                        Console.WriteLine("Ingrese el numero del contacto que desea editar");
+                        int Contacto = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        Console.WriteLine("Como prefiere nombrar este contacto");
+                        string NewContacto = Console.ReadLine();
+                        Contactos[Contacto - 1] = NewContacto;
+                        Console.WriteLine("Que nuevo numero telefonico desea añadir a este contacto");
+                        string NewTelefono = Console.ReadLine();
+                        NumDC[Contacto - 1] = NewTelefono;
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 4:
+
+                        Console.Clear();
+                        for (int i = 0; i < Contactos.Length; i++)
+                        {
+                            string elemento = Contactos[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento);
+                        }
+                        Console.WriteLine("\n");
+                        for (int i = 0; i < NumDC.Length; i++)
+                        {
+                            string elemento2 = NumDC[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento2);
+
+                        }
+                        Console.WriteLine("Ingrese el numero del contacto que desea eliminar");
+                        int Contacto2 = int.Parse(Console.ReadLine());
+                        Contactos[Contacto2 - 1] = "";
+                        NumDC[Contacto2 - 1] = "";
+                        Console.Clear();
+
+                        break;
+
+                    case 5:
+                        close = true;
+
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Esa opcion no se encuentra disponible");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
+                if (close)
+                {
+                    break;
+
+
+                }
+
+
+
+
+
+
+            }
+                
+        }
+
+            
+    }
+}
+
---
+++ Tarea.3.2/Program.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ListFrutas = new string[10];
+            string[] ListVegetales = new string[10];
+            string[] ListLacteos = new string[10];
+
+
+            while (true)
+            {
+                bool close = false;
+                Console.WriteLine("~Lista de compra~\n" +
+                "\n[1] Añadir producto a una lista\n" + "[2] Mostrar listas\n" + "[3] Editar producto de una lista\n" + "[4] Eliminar producto de una lista\n" + "[5] Cerrar programa");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Elija a que lista desea añadir el producto\n" +
+                        "\n[1] Frutas\n" + "[2] Vegetales\n" + "[3] Lacteos");
+                        int opcion2 = int.Parse(Console.ReadLine());
+                        switch (opcion2)
+                        {
+                            case 1:
+
+                                if (string.IsNullOrEmpty(ListFrutas[9]))
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Ingrese el producto");
+                                    string Fruta = Console.ReadLine();
+
+                                    for (int i = 0; i < ListFrutas.Length; i++)
+                                    {
+                                        string elemento = ListFrutas[i];
+
+                                        if (string.IsNullOrEmpty(elemento))
+                                        {
+                                            ListFrutas[i] = Fruta;
+                                            Console.Clear();
+                                            break;
+                                            
+                                        }
+
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No hay mas espacio disponible");
+                                    Console.ReadKey();
+                                    break;
+                                }
+                                break;
+                            case 2:
+                                if (string.IsNullOrEmpty(ListVegetales[9]))
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Ingrese el producto");
+                                    string Vegetal = Console.ReadLine();
+
+                                    for (int i = 0; i < ListVegetales.Length; i++)
+                                    {
+                                        string elemento = ListVegetales[i];
+
+                                        if (string.IsNullOrEmpty(elemento))
+                                        {
+                                            ListVegetales[i] = Vegetal;
+                                            Console.Clear();
+                                            break;
+                                        }
+
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No hay mas espacio disponible");
+                                    Console.ReadKey();
+                                    break;
+                                }
+                                break;
+                            case 3:
+
+                                if (string.IsNullOrEmpty(ListLacteos[9]))
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Ingrese el producto");
+                                    string Lacteo = Console.ReadLine();
+
+                                    for (int i = 0; i < ListLacteos.Length; i++)
+                                    {
+                                        string elemento = ListLacteos[i];
+
+                                        if (string.IsNullOrEmpty(elemento))
+                                        {
+                                            ListLacteos[i] = Lacteo;
+                                            Console.Clear();
+                                            break;
+                                            
+                                        }
+
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No hay mas espacio disponible");
+                                    Console.ReadKey();
+                                    break;
+                                }
+                                break;
+                                
+                        }
+                        break;
+                       
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("LISTA DE FRUTAS\n");
+                        for (int i = 0; i < ListFrutas.Length; i++)
+                        {
+                            string elemento = ListFrutas[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento);
+                        }
+                        Console.WriteLine("\nLISTA DE VEGETALES\n");
+                        for (int i = 0; i < ListVegetales.Length; i++)
+                        {
+                            string elemento = ListVegetales[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento);
+                        }
+                        Console.WriteLine("\nLISTA DE LACTEOS\n");
+                        for (int i = 0; i < ListLacteos.Length; i++)
+                        {
+                            string elemento = ListLacteos[i];
+
+                            Console.WriteLine((i + 1) + "-" + elemento);
+                        }                        
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Elija en cual lista se encuentra el producto que desea editar\n" +
+                        "\n[1] Frutas\n" + "[2] Vegetales\n" + "[3] Lacteos");
+                        int opcion3 = int.Parse(Console.ReadLine());
+
+                        switch(opcion3)
+                        {
+                            case 1:
+                                Console.Clear();
+                                for (int i = 0; i < ListFrutas.Length; i++)
+                                {
+                                    string elemento = ListFrutas[i];
+
+                                    Console.WriteLine((i + 1) + "-" + elemento);
+                                }                             
+                                Console.WriteLine("Ingrese el numero del producto que desea editar");
+                                int Producto1 = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Como prefiere nombrar este producto");
+                                string NewProducto1 = Console.ReadLine();
+                                ListFrutas[Producto1 - 1] = NewProducto1;
+                                Console.Clear();
+                                break;
+                            case 2:
+                                Console.Clear();
+                                for (int i = 0; i < ListVegetales.Length; i++)
+                                {
+                                    string elemento = ListVegetales[i];
+
+                                    Console.WriteLine((i + 1) + "-" + elemento);
+                                }
+                                Console.WriteLine("Ingrese el numero del producto que desea editar");
+                                int Producto2 = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Como prefiere nombrar este producto");
+                                string NewProducto2 = Console.ReadLine();
+                                ListVegetales[Producto2 - 1] = NewProducto2;
+                                Console.Clear();
+                                break;
+                            case 3:
+                                Console.Clear();
+                                for (int i = 0; i < ListLacteos.Length; i++)
+                                {
+                                    string elemento = ListLacteos[i];
+
+                                    Console.WriteLine((i + 1) + "-" + elemento);
+                                }
+                                Console.WriteLine("Ingrese el numero del producto que desea editar");
+                                int Producto3 = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Como prefiere nombrar este producto");
+                                string NewProducto3 = Console.ReadLine();
+                                ListLacteos[Producto3 - 1] = NewProducto3;
+                                Console.Clear();
+                                break;
+                        }
+
+                        break;
+
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Elija en cual lista se encuentra el producto que desea eliminar\n" +
+                        "\n[1] Frutas\n" + "[2] Vegetales\n" + "[3] Lacteos");
+                        int opcion4 = int.Parse(Console.ReadLine());
+                        
+                        switch(opcion4)
+                        {
+                            case 1:
+                                Console.Clear();
+                                for (int i = 0; i < ListFrutas.Length; i++)
+                                {
+                                    string elemento = ListFrutas[i];
+
+                                    Console.WriteLine((i + 1) + "-" + elemento);
+                                }
+                                Console.WriteLine("Ingrese el numero del prpoducto que desea eliminar");
+                                int Producto1 = int.Parse(Console.ReadLine());
+                                ListFrutas[Producto1 - 1] = "";
+                                Console.Clear();
+                                break;
+                            case 2:
+                                Console.Clear();
+                                for (int i = 0; i < ListVegetales.Length; i++)
+                                {
+                                    string elemento = ListVegetales[i];
+
+                                    Console.WriteLine((i + 1) + "-" + elemento);
+                                }
+                                Console.WriteLine("Ingrese el numero del prpoducto que desea eliminar");
+                                int Producto2 = int.Parse(Console.ReadLine());
+                                ListVegetales[Producto2 - 1] = "";
+                                Console.Clear();
+                                break;
+                            case 3:
+                                Console.Clear();
+                                for (int i = 0; i < ListLacteos.Length; i++)
+                                {
+                                    string elemento = ListLacteos[i];
+
+                                    Console.WriteLine((i + 1) + "-" + elemento);
+                                }
+                                Console.WriteLine("Ingrese el numero del producto que desea eliminar");
+                                int Producto3 = int.Parse(Console.ReadLine());
+                                ListLacteos[Producto3 - 1] = "";
+                                Console.Clear();
+                                break;
+                        }
+                        break;
+
+                    case 5:
+                       close = true;
+                        
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Esa opcion no se encuentra disponible");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                        
+                }
+
+
+                
+
+                if (close)
+                {
+                    break;
+                } 
+            }
+
+             
+        }
+    }
+}
